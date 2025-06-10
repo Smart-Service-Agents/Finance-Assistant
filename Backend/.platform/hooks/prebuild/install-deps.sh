48dacc88cb6b1be9fdea8f4e4721670ff6034455
shreyansh-rey <shreyanshbanerjee6@gmail.com>
2025-06-10 10:36:11 +0530

    Update install-deps.sh

--- Backend/.platform/hooks/prebuild/install-deps.sh
+++ Backend/.platform/hooks/prebuild/install-deps.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Installing dependencies from requirements.txt..."
-/usr/bin/python3.11 -m pip install -r requirements.txt
+source /var/app/venv/*/bin/activate
+pip install -r requirements.txt
