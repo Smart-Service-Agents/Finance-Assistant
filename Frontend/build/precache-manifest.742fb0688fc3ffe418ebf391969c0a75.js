141992a6d141b51a8e0853690cc89f88cf75c867
Rajeev <apple@Apples-MacBook-Pro.local>
2025-07-05 18:10:44 +0530

    updates requirements.txt

---
+++ Frontend/build/precache-manifest.742fb0688fc3ffe418ebf391969c0a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a32600cb47ca95ed6b82086a65a3bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2b3e24e49eddaf6e5ac",
+    "url": "/static/css/2.b0aab389.chunk.css"
+  },
+  {
+    "revision": "9a0ffd5570b388716f73",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d2b3e24e49eddaf6e5ac",
+    "url": "/static/js/2.df618846.chunk.js"
+  },
+  {
+    "revision": "1299ed581ef47dcd8388bb4526f15704",
+    "url": "/static/js/2.df618846.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12c20aea6425d957e09",
+    "url": "/static/js/3.f070c740.chunk.js"
+  },
+  {
+    "revision": "9a0ffd5570b388716f73",
+    "url": "/static/js/main.45125947.chunk.js"
+  },
+  {
+    "revision": "06b30d75a5461f9c21cd",
+    "url": "/static/js/runtime-main.11441e22.js"
+  }
+]);
